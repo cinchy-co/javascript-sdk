eca243d99513b4fdc426227a8c8f126fd05c3db7
yogesh.jagtap <yogesh.jagtap@cinchy.com>
2022-08-18 15:09:07 +0530

    Added setAuthorizationHeader function which add bearer token for every outgoing request.

--- cinchy.js
+++ cinchy.js
@@ -17082,11 +17082,6 @@ function CinchyJS(_options) {
 
     function openConnection(successCallback, errorCallback, callbackState) {
         var errorMsg = 'Failed to open connection';
-        var beforeSendFn = function (xhr) {
-            if (_usr && _usr.access_token) {
-                xhr.setRequestHeader('Authorization', 'Bearer ' + _usr.access_token);
-            }
-        };
         var successFn = function (data) {
             var connectionId = data;
             if (isFunction(successCallback))
@@ -17107,7 +17102,7 @@ function CinchyJS(_options) {
             url: _options.cinchyRootUrl + '/API/OpenConnection',
             type: 'GET',
             dataType: 'text',
-            beforeSend: beforeSendFn,
+            beforeSend: setAuthorizationHeader(),
             success: successFn,
             error: errorFn
         });
@@ -17118,11 +17113,7 @@ function CinchyJS(_options) {
             return;
         var errorMsg = 'Failed to close connection ' + connectionId;
         var form_data = getFormUrlEncodedData({ "connectionId": connectionId });
-        var beforeSendFn = function (xhr) {
-            if (_usr && _usr.access_token) {
-                xhr.setRequestHeader('Authorization', 'Bearer ' + _usr.access_token);
-            }
-        };
+
         var successFn = function (data) {
             if (isFunction(successCallback))
                 successCallback(connectionId, callbackState);
@@ -17143,7 +17134,7 @@ function CinchyJS(_options) {
             type: 'POST',
             data: form_data,
             dataType: 'text',
-            beforeSend: beforeSendFn,
+            beforeSend: setAuthorizationHeader(),
             success: successFn,
             error: errorFn
         });
@@ -17154,11 +17145,7 @@ function CinchyJS(_options) {
             return null;
         var errorMsg = 'Failed to begin transaction on connection ' + connectionId;
         var form_data = getFormUrlEncodedData({ "connectionId": connectionId });
-        var beforeSendFn = function (xhr) {
-            if (_usr && _usr.access_token) {
-                xhr.setRequestHeader('Authorization', 'Bearer ' + _usr.access_token);
-            }
-        };
+
         var successFn = function (data) {
             var transactionId = data;
             if (isFunction(successCallback))
@@ -17180,7 +17167,7 @@ function CinchyJS(_options) {
             type: 'POST',
             data: form_data,
             dataType: 'text',
-            beforeSend: beforeSendFn,
+            beforeSend: setAuthorizationHeader(),
             success: successFn,
             error: errorFn
         });
@@ -17191,11 +17178,7 @@ function CinchyJS(_options) {
             return null;
         var errorMsg = 'Failed to commit transaction ' + transactionId + ' on connection ' + connectionId;
         var form_data = getFormUrlEncodedData({ "connectionId": connectionId, "transactionId": transactionId });
-        var beforeSendFn = function (xhr) {
-            if (_usr && _usr.access_token) {
-                xhr.setRequestHeader('Authorization', 'Bearer ' + _usr.access_token);
-            }
-        };
+
         var successFn = function (data) {
             if (isFunction(successCallback))
                 successCallback(connectionId, transactionId, callbackState);
@@ -17216,7 +17199,7 @@ function CinchyJS(_options) {
             type: 'POST',
             data: form_data,
             dataType: 'text',
-            beforeSend: beforeSendFn,
+            beforeSend: setAuthorizationHeader(),
             success: successFn,
             error: errorFn
         });
@@ -17227,11 +17210,7 @@ function CinchyJS(_options) {
             return null;
         var errorMsg = 'Failed to rollback transaction ' + transactionId + ' on connection ' + connectionId;
         var form_data = getFormUrlEncodedData({ "connectionId": connectionId, "transactionId": transactionId });
-        var beforeSendFn = function (xhr) {
-            if (_usr && _usr.access_token) {
-                xhr.setRequestHeader('Authorization', 'Bearer ' + _usr.access_token);
-            }
-        };
+
         var successFn = function (data) {
             if (isFunction(successCallback))
                 successCallback(connectionId, transactionId, callbackState);
@@ -17252,7 +17231,7 @@ function CinchyJS(_options) {
             type: 'POST',
             data: form_data,
             dataType: 'text',
-            beforeSend: beforeSendFn,
+            beforeSend: setAuthorizationHeader(),
             success: successFn,
             error: errorFn
         });
@@ -17263,11 +17242,7 @@ function CinchyJS(_options) {
         if (isNonNullObject(params)) {
             form_data = getFormUrlEncodedData(params);
 				}
-        var beforeSendFn = function (xhr) {
-            if (_usr && _usr.access_token) {
-								xhr.setRequestHeader('Authorization', 'Bearer ' + _usr.access_token);
-            }
-        };
+
         var successFn = function (data) {
             var jsonQueryResult = new JsonQueryResult(data);
             if (isFunction(successCallback))
@@ -17300,7 +17275,7 @@ function CinchyJS(_options) {
                 type: 'POST',
                 data: form_data,
                 dataType: 'json',
-	            beforeSend: beforeSendFn,
+	            beforeSend: setAuthorizationHeader(),
                 success: successFn,
                 error: errorFn
             });
@@ -17309,7 +17284,7 @@ function CinchyJS(_options) {
                 url: apiUrl,
                 type: 'POST',
                 dataType: 'json',
-		        beforeSend: beforeSendFn,
+		        beforeSend: setAuthorizationHeader(),
                 success: successFn,
                 error: errorFn
             });
@@ -17337,67 +17312,55 @@ function CinchyJS(_options) {
         if (!isNonNullOrWhitespaceString(_options.client_id))
             throw new CinchyException('options.client_id is a mandatory field');
     }
-
-    function getGroupsCurrentUserBelongsTo() {
+    function setAuthorizationHeader(){
 		var beforeSendFn = function (xhr) {
 			if (_usr && _usr.access_token) {
 				xhr.setRequestHeader('Authorization', 'Bearer ' + _usr.access_token);
 			}
 		};
+		return beforeSendFn;
+	}
+    function getGroupsCurrentUserBelongsTo() {
+		
 		var apiUrl = _options.cinchyRootUrl + '/Account/GetGroupsCurrentUserBelongsTo';
         return $.ajax({
             url: apiUrl,
             type: 'GET',
             dataType: 'json',
-			beforeSend: beforeSendFn
+			beforeSend: setAuthorizationHeader()
         });
     }
 
     function getTableEntitlementsById(tableId) {
-		var beforeSendFn = function (xhr) {
-			if (_usr && _usr.access_token) {
-				xhr.setRequestHeader('Authorization', 'Bearer ' + _usr.access_token);
-			}
-		};
         var apiUrl = _options.cinchyRootUrl + '/Account/GetTableEntitlementsById';
         return $.ajax({
             url: apiUrl,
             type: 'POST',
             data: { "tableId": tableId },
             dataType: 'json',
-			beforeSend: beforeSendFn
+			beforeSend: setAuthorizationHeader()
         });
     }
 
     function getTableEntitlementsByGuid(tableGuid) {
-		var beforeSendFn = function (xhr) {
-			if (_usr && _usr.access_token) {
-				xhr.setRequestHeader('Authorization', 'Bearer ' + _usr.access_token);
-			}
-		};
         var apiUrl = _options.cinchyRootUrl + '/Account/GetTableEntitlementsByGuid';
         return $.ajax({
             url: apiUrl,
             type: 'POST',
             data: { "tableGuid": tableGuid },
             dataType: 'json',
-			beforeSend: beforeSendFn
+			beforeSend: setAuthorizationHeader()
         });
     }
 
     function getTableEntitlementsByName(domainName, tableName) {
-		var beforeSendFn = function (xhr) {
-			if (_usr && _usr.access_token) {
-				xhr.setRequestHeader('Authorization', 'Bearer ' + _usr.access_token);
-			}
-		};
         var apiUrl = _options.cinchyRootUrl + '/Account/GetTableEntitlementsByName';
         return $.ajax({
             url: apiUrl,
             type: 'POST',
             data: { "domainName": domainName, "tableName": tableName },
             dataType: 'json',
-			beforeSend: beforeSendFn
+			beforeSend: setAuthorizationHeader()
 	    });
     }
 
@@ -17436,11 +17399,7 @@ function CinchyJS(_options) {
 					'debug': debug
 				});
 				var errorMsg = 'Failed to retrieve translated literals.';
-				var beforeSendFn = function (xhr) {
-					if (_usr && _usr.access_token) {
-						xhr.setRequestHeader('Authorization', 'Bearer ' + _usr.access_token);
-					}
-				};
+
 				var successFn = function (data) {
 					var jsonObj = JSON.parse(data);
 					if (isFunction(successCallback))
@@ -17462,7 +17421,7 @@ function CinchyJS(_options) {
 					type: 'POST',
 					data: form_data,
 					dataType: 'text',
-					beforeSend: beforeSendFn,
+					beforeSend: setAuthorizationHeader(),
 					success: successFn,
 					error: errorFn
 				});
